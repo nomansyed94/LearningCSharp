6695be17b2ca6fd6a964f0ce4cf8ce029a5401cc
noman syed <noman-syed@hotmail.com>
2019-05-07 17:20:03 +0200

    Second commit

---
+++ test4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 1-10: ");
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+            
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
